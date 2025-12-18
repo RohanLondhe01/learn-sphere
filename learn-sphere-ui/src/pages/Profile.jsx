1d6f511d202f2856262c25005d8d8fa397360e84
Rohit913091 <RohitSantosh.Gavakare@cognizant.com>
2025-12-18 15:32:16 +0530

    Adding Academic info

--- learn-sphere-ui/src/pages/Profile.jsx
+++ learn-sphere-ui/src/pages/Profile.jsx
@@ -1,9 +1,12 @@
 import { PersonalInfo } from '../components/PersonalInfo'
+import { PAcademic } from '../components/PAcademic'
+
 
 export const Profile = () => {
   return (
     <>
     <PersonalInfo/>
+    <PAcademic/>
     </>
   )
 }
